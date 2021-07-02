25a44bdea3eb421f0a2208ef54aed10592a5a3e3
agnijaarmane <agnijaarmane@gmail.com>
2021-07-02 21:49:37 +0300

    first commit

---
+++ src/HeroApplication.java
@@ -0,0 +1,60 @@
+public class HeroApplication {
+
+    public static void main(String[] args) {
+// Variable initialization
+        String heroName;
+        String heroCity;
+
+        String heroPower1, heroPower2, heroPower3;
+
+        boolean isEvil;
+
+        double heroSalary;
+        String realName;
+
+        String universe;
+// Variable assignment
+      heroName = "Express";
+      heroCity = "Marupe";
+      heroPower1 = "Super thin";
+      heroPower2 = "Fast running";
+      heroPower3 = "eating";
+
+      isEvil = false;
+      heroSalary = 99.99;
+      realName = "Agnija";
+      universe = "Ours";
+
+     // Print out Hero information card
+     System.out.println("**");
+
+     System.out.println("Hero name: " + heroName);
+     System.out.println("This hero lives in: " + heroCity);
+
+     System.out.println("Hero Superpowers");
+     System.out.println("___" +heroPower1);
+     System.out.println("___" +heroPower2);
+     System.out.println("___" +heroPower3);
+
+     System.out.println("This super hero is evil?" +isEvil);
+             System.out.println("This hero earns: " + heroSalary + "EUR");
+
+            System.out.println("This hero earns: " + heroSalary + " EUR");
+     System.out.println( realName + " hides his identity.");
+     System.out.println("hero lives in " + universe + "universe");
+
+     System.out.println("*******");
+     System.out.println("*** HERO INFORMATION ***");
+     System.out.println("********");
+
+
+
+
+
+
+
+
+
+
+    }
+}
